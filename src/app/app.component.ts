8f252d455bde2613602c7cd7d0f8d236da8a71c0
heynikhil <nsavaliya17@gmail.com>
2023-12-05 09:09:01 +0530

    UI changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { TitleComponent } from './title/title.component';
+import { DividerComponent } from './divider/divider.component';
+import { HeadlinesComponent } from './headlines/headlines.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent, TitleComponent, DividerComponent, HeadlinesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'other-party-ui-brut';
+  title = 'Other Party';
 }
---
+++ src/app/headlines/headlines.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headlines',
+  standalone: true,
+  imports: [],
+  templateUrl: './headlines.component.html',
+  styleUrl: './headlines.component.scss'
+})
+export class HeadlinesComponent {
+
+}
