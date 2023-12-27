de3b13fd746eceae4ae9078e774def745657f260
heynikhil <nsavaliya17@gmail.com>
2023-12-27 09:48:23 +0530

    scroll

--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -2,24 +2,59 @@ import { NgClass, NgFor, NgIf } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component, ViewEncapsulation } from '@angular/core';
 import { BillService } from '../../services/bill.service';
+import { InfiniteScrollModule } from "ngx-infinite-scroll";
 
 @Component({
   selector: 'app-stories',
   standalone: true,
-  imports: [HttpClientModule, NgIf, NgFor, NgClass],
+  imports: [HttpClientModule, NgIf, NgFor, NgClass, InfiniteScrollModule],
   templateUrl: './stories.component.html',
   styleUrl: './stories.component.scss',
 })
 export class StoriesComponent {
   public stories: any;
   public headLines: any;
+  public isLoading: any;
+  throttle = 300;
+  scrollDistance = 1;
+  scrollUpDistance = 2;
 
-  constructor(private headLineService: BillService) {}
+  currentPage: number = 0;
+  itemsPerPage: number = 15;
+
+  constructor(private headLineService: BillService) { }
 
   ngOnInit() {
-    this.headLineService.getHeadLines(15, 0, 'DESC').subscribe((response) => {
-      this.headLines = response?.data?.stories.slice(0, 5);
-      this.stories = response?.data?.stories.slice(5, 15);
-    });
+    this.loadData('init')
   }
+
+  public loadData = (type?: string) => {
+    this.toggleLoading();
+    this.headLineService.getHeadLines(this.itemsPerPage, this.currentPage, 'DESC').subscribe({
+      next: response => {
+        if (type) this.headLines = response?.data?.stories.slice(0, 5);
+        if (type) this.stories = response?.data?.stories.slice(5, 15);
+        else this.stories = response?.data?.stories
+      },
+      error: err => console.log(err),
+      complete: () => this.toggleLoading()
+    })
+  }
+
+  toggleLoading = () => this.isLoading = !this.isLoading;
+
+  appendData = () => {
+    this.toggleLoading();
+    this.headLineService.getHeadLines(this.itemsPerPage, this.currentPage, 'DESC').subscribe({
+      next: response => this.stories = [...this.stories, ...response?.data?.stories],
+      error: err => console.log(err),
+      complete: () => this.toggleLoading()
+    })
+  }
+
+  onScroll = () => {
+    this.currentPage++;
+    this.appendData();
+  }
+
 }
