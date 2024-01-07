f0309747ff1fcafe2d62f5a90a0a9a7a217138b2
heynikhil <nsavaliya17@gmail.com>
2024-01-07 09:30:13 +0530

    work on layout

--- src/app/full-story/full-story.component.ts
+++ src/app/full-story/full-story.component.ts
@@ -1,4 +1,4 @@
-import { NgClass, NgFor, NgIf } from '@angular/common';
+import { DatePipe, NgClass, NgFor, NgIf } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { BillService } from '../../services/bill.service';
@@ -9,12 +9,13 @@ import { TitleComponent } from '../title/title.component';
 import { DividerComponent } from '../divider/divider.component';
 import { FooterComponent } from '../footer/footer.component';
 import { DomSanitizer } from '@angular/platform-browser';
+import { ContentComponent } from '../stories/content/content.component';
 
 
 @Component({
   selector: 'app-full-story',
   standalone: true,
-  imports: [HttpClientModule, NgIf, NgFor, NgClass, InfiniteScrollModule, RouterModule, NavbarComponent, TitleComponent, DividerComponent, FooterComponent],
+  imports: [HttpClientModule, NgIf, NgFor, NgClass, InfiniteScrollModule, RouterModule, NavbarComponent, TitleComponent, DividerComponent, FooterComponent, DatePipe, ContentComponent],
   templateUrl: './full-story.component.html',
   styleUrl: './full-story.component.scss'
 })
@@ -24,30 +25,25 @@ export class FullStoryComponent implements OnInit {
   public billSummery: any;
 
   constructor(private billService: BillService, private route: ActivatedRoute, private sanitizer: DomSanitizer) {
-    this.route.params.subscribe(params => {
-      this._id = params['id'];
-    });
+ 
   }
 
   ngOnInit(): void {
-
-    console.log(this._id);
+    
+    this.route.params.subscribe(params => {
+      this._id = params['id'];
+    });
+    
     if (this._id) {
       this.billService.getFullStory(this._id).subscribe((data) => {
         this.bill = data.data.bill;
         this.billSummery = data.data.billSummery;
-        console.log(this.bill);
-
       })
-
     }
-
   }
 
   sanitize(url: string) {
     return this.sanitizer.bypassSecurityTrustUrl(url);
   }
-
-
 
 }
--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -8,100 +8,19 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { TitleComponent } from '../title/title.component';
 import { DividerComponent } from '../divider/divider.component';
 import { FooterComponent } from '../footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-stories',
   standalone: true,
-  imports: [HttpClientModule, NgIf, NgFor, NgClass, InfiniteScrollModule, RouterModule, NavbarComponent, TitleComponent, DividerComponent, FooterComponent],
+  imports: [HttpClientModule, NgIf, NgFor, NgClass, InfiniteScrollModule, RouterModule, NavbarComponent, TitleComponent, DividerComponent, FooterComponent, ContentComponent],
   templateUrl: './stories.component.html',
   styleUrl: './stories.component.scss',
 })
 export class StoriesComponent {
-  public stories: any;
-  public headLines: any;
-  public isLoading: any;
-  throttle = 300;
-  scrollDistance = 1;
-  scrollUpDistance = 2;
-
-  currentPage: number = 0;
-  itemsPerPage: number = 15;
 
   constructor(private headLineService: BillService) { }
 
   ngOnInit() {
-    this.loadData('init')
   }
-
-  public loadData = (type?: string) => {
-    this.toggleLoading();
-    this.headLineService.getHeadLines(this.itemsPerPage, this.currentPage, 'DESC').subscribe({
-      next: response => {
-        if (type) this.headLines = response?.data?.stories.slice(0, 5);
-        if (type) this.stories = response?.data?.stories.slice(5, 15);
-        else this.stories = response?.data?.stories
-
-        const classes = ['half', 'third', 'full', 'fourth'];
-        this.stories =  this.assignClassesToStories(this.stories, classes);
-      },
-      error: err => console.log(err),
-      complete: () => this.toggleLoading()
-    })
-  }
-
-  toggleLoading = () => this.isLoading = !this.isLoading;
-
-  appendData = () => {
-    this.toggleLoading();
-    this.headLineService.getHeadLines(this.itemsPerPage, this.currentPage, 'DESC').subscribe({
-      next: response => {
-        this.stories = [...this.stories, ...response?.data?.stories];
-        const classes = ['half', 'third', 'full', 'fourth'];
-        this.stories =  this.assignClassesToStories(this.stories, classes);
-      },
-      error: err => console.log(err),
-      complete: () => this.toggleLoading()
-    })
-  }
-
-  onScroll = () => {
-    this.currentPage++;
-    this.appendData();
-  }
-
-   assignClassesToStories(array: any, classes:any) {
-    let currentIndex = 0;
-  
-    for (let i = 1; i < array.length; ) {
-      const currentClass = classes[currentIndex];
-      let increment = 0;
-  
-      if (currentClass === 'third') {
-        increment = 3; // For 'third' class, assign to 3 consecutive elements
-      } else if (currentClass === 'half') {
-        increment = 2; // For other classes, assign to 1 element
-      } else if (currentClass === 'full') {
-        increment = 1;
-      } else if (currentClass === 'fourth') {
-        increment = 4;
-      }
-  
-      // Assign the current class to the next group of elements
-      for (let j = 0; j < increment && i < array.length; j++) {
-        array[i].className = currentClass;
-        i++;
-      }
-  
-      // Move to the next class index based on the rules
-      currentIndex = (currentIndex + 1) % classes.length;
-    }
-  
-    return array;
-  }
-
-  openTwitter(username: string) {
-    const url = `https://twitter.com/intent/tweet?screen_name=${username}&ref_src=twsrc%5Etfw`
-    window.open(url, '_blank');
-  }
-
 }
