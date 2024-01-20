61def1b7489a5ab1495f749737e278cbc995818c
aflowNYC <seth@verbalagency.com>
2024-01-20 13:07:13 -0700

    button update for govtrack

--- src/app/full-story/full-story.component.ts
+++ src/app/full-story/full-story.component.ts
@@ -2,7 +2,7 @@ import { DatePipe, NgClass, NgFor, NgIf } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { BillService } from '../../services/bill.service';
-import { InfiniteScrollModule } from "ngx-infinite-scroll";
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { ActivatedRoute, Route, RouterModule } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { TitleComponent } from '../title/title.component';
@@ -11,34 +11,47 @@ import { FooterComponent } from '../footer/footer.component';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ContentComponent } from '../stories/content/content.component';
 
-
 @Component({
   selector: 'app-full-story',
   standalone: true,
-  imports: [HttpClientModule, NgIf, NgFor, NgClass, InfiniteScrollModule, RouterModule, NavbarComponent, TitleComponent, DividerComponent, FooterComponent, DatePipe, ContentComponent],
+  imports: [
+    HttpClientModule,
+    NgIf,
+    NgFor,
+    NgClass,
+    InfiniteScrollModule,
+    RouterModule,
+    NavbarComponent,
+    TitleComponent,
+    DividerComponent,
+    FooterComponent,
+    DatePipe,
+    ContentComponent,
+  ],
   templateUrl: './full-story.component.html',
-  styleUrl: './full-story.component.scss'
+  styleUrl: './full-story.component.scss',
 })
 export class FullStoryComponent implements OnInit {
   public _id: any;
   public bill: any;
   public billSummery: any;
 
-  constructor(private billService: BillService, private route: ActivatedRoute, private sanitizer: DomSanitizer) {
- 
-  }
+  constructor(
+    private billService: BillService,
+    private route: ActivatedRoute,
+    private sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit(): void {
-    
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       this._id = params['id'];
     });
-    
+
     if (this._id) {
       this.billService.getFullStory(this._id).subscribe((data) => {
         this.bill = data.data.bill;
         this.billSummery = data.data.billSummery;
-      })
+      });
     }
   }
 
@@ -47,8 +60,12 @@ export class FullStoryComponent implements OnInit {
   }
 
   openTwitter(username: string) {
-    const url = `https://twitter.com/intent/tweet?screen_name=${username}&ref_src=twsrc%5Etfw`
+    const url = `https://twitter.com/intent/tweet?screen_name=${username}&ref_src=twsrc%5Etfw`;
     window.open(url, '_blank');
   }
 
+  openGovTrack(link: string) {
+    const url = `${link}`;
+    window.open(url, '_blank');
+  }
 }
