781101fa6dce18207c28517eb44b00ef0211c4ab
aflowNYC <seth@verbalagency.com>
2024-08-15 00:08:26 -0600

    nested and tree-list

--- src/assets/core/jab/buix.js
+++ src/assets/core/jab/buix.js
@@ -54,3 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
 /* character counter 
 https://codepen.io/stamd/pen/BaJJrdq
 */
+
+// Tree List
+var toggler = document.getElementsByClassName("parent"); var i;
+for (i = 0; i < toggler.length; i++) {toggler[i].addEventListener("click", function() {this.parentElement.querySelector(".nested").classList.toggle("active");this.classList.toggle("parent-down");});}
