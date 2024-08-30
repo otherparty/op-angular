55a442a4eb168aa85ceb9e97906dc9ef642e7b54
Nikhil <nikhil.savaliya@freshprints.com>
2024-08-30 09:45:32 +0530

    Fix

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,10 +1,11 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, ViewChild } from '@angular/core';
 import { DividerComponent } from '../divider/divider.component';
 import { ContentComponent } from '../stories/content/content.component';
 import { BillService } from '../../services/bill.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { isPlatformServer } from '@angular/common';
 @Component({
   selector: 'app-title',
   standalone: true,
@@ -26,12 +27,27 @@ export class TitleComponent {
     { name: "Healthcare", checked: false },
     { name: "Environment", checked: false },
   ]
-  constructor(private formBuilder: FormBuilder, private api: BillService) {
+  constructor(private formBuilder: FormBuilder, private api: BillService, @Inject(PLATFORM_ID) private _platformId: Object,
+  ) {
 
+
+    if (isPlatformServer(this._platformId)) {
+      return;
+    } else {
+      const searchString = sessionStorage.getItem('search');
+      if (searchString) {
+        const lastSearchedTab = this.tabs.find(t => t.name === searchString)
+        if (lastSearchedTab) {
+          this.tabs.map(t => {
+            t.checked = t.name === lastSearchedTab.name ? true : false
+          })
+        }
+      }
+    }
   }
 
   getDataBasedOnTags(tab: any) {
-    this.tabs.map(t=> {
+    this.tabs.map(t => {
       t.checked = t.name === tab.name ? true : false
     })
     this.api.callXFunction(tab);
