a3cbe9cc9ac7b24b97dfbf6cbec5767760f96c45
aflowNYC <seth@verbalagency.com>
2024-12-05 21:41:03 -0700

    ui updates

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,6 +5,7 @@ import { TitleComponent } from '../title/title.component';
 import { DividerComponent } from '../divider/divider.component';
 import { FooterComponent } from '../footer/footer.component';
 import { ContentComponent } from '../stories/content/content.component';
+import { AuthenticateService } from '../../services/cognito.service';
 
 @Component({
   selector: 'app-about',
@@ -14,5 +15,13 @@ import { ContentComponent } from '../stories/content/content.component';
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
+  user: any;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private readonly cognito: AuthenticateService    
+  ) {
+    console.log(this.router.url);
+    this.user = this.cognito.getUser();
+  }
 }
