46914068edf47be015fdbe2f364c2e6ce47263cc
aflowNYC <seth@verbalagency.com>
2024-12-07 20:06:27 -0700

    large-thumb experiment

--- dist/server/main.server.mjs
+++ dist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{b as a}from"./chunk-RPSYMVM3.mjs";import"./chunk-LOEXHLJO.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
+import{b as a}from"./chunk-UWGYD54I.mjs";import"./chunk-LOEXHLJO.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
