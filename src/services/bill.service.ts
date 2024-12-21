430cd689ea2ef728dc63a170e5d7274d69797f50
aflowNYC <seth@verbalagency.com>
2024-12-20 21:21:48 -0700

    url update for prod

--- src/services/bill.service.ts
+++ src/services/bill.service.ts
@@ -7,10 +7,10 @@ import { Observable, Subject, catchError, of, tap } from 'rxjs';
   providedIn: 'root',
 })
 export class BillService {
-  billsURL = 'http://localhost:9000/api/v1/stories'; // URL to web api
-  baseURL = 'http://localhost:9000/api/v1'; // URL to web api
-  // baseURL = 'https://backend.otherparty.ai/api/v1/'; // URL to web api
-  // billsURL = `https://backend.otherparty.ai/api/v1/stories`;
+  // billsURL = 'http://localhost:9000/api/v1/stories'; // URL to web api
+  // baseURL = 'http://localhost:9000/api/v1'; // URL to web api
+  baseURL = 'https://backend.otherparty.ai/api/v1/'; // URL to web api
+  billsURL = `https://backend.otherparty.ai/api/v1/stories`;
 
   private xFunctionSubject = new Subject<[any, boolean]>();
   private yFunctionSubject = new Subject<[any, boolean]>();
