9e12412a3ec8315f54de21627ef2835685cfd8df
Nikhil Savaliya <nsavaliya17@gmail.com>
2025-01-29 10:20:16 +0530

    button

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -4,18 +4,24 @@ import { RouterModule } from '@angular/router';
 import { AuthenticateService } from '../../services/cognito.service';
 import { PrivacyPolicyComponent } from '../privacy-policy/privacy-policy.component';
 import { TermsAndConditions } from '../terms-and-conditions/terms-and-conditions.component';
+import { BillService } from '../../services/bill.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [RouterModule, PrivacyPolicyComponent, TermsAndConditions],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   user: any;
- 
-  constructor(private readonly cognito: AuthenticateService) {
+
+  constructor(
+    private toastr: ToastrService,
+    private readonly cognito: AuthenticateService,
+    private readonly userService: BillService
+  ) {
     this.user = this.cognito.getUser();
   }
 
@@ -23,4 +29,13 @@ export class NavbarComponent {
     this.cognito.logOut();
   }
 
+  public cancelSubscription() {
+    this.cognito.getIdToken().then((idToken) => {
+      this.userService
+        .cancelForUserSubscription(idToken || '')
+        .subscribe((response) => {
+          this.toastr.success('User verified successfully', 'Success');
+        });
+    });
+  }
 }
